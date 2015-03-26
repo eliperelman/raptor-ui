9b0d417dc51e855479f30589fd814b499f320bd4
Eli Perelman <eli@eliperelman.com>
2015-03-25 19:00:01 -0500

    Initial commit

---
+++ config.js
@@ -0,0 +1,29 @@
+define(['settings'], function (Settings) {
+  return new Settings({
+    datasources: {
+      raptor: {
+        default: true,
+        type: 'influxdb',
+        url: 'http://goldiewilson-onepointtwentyone-1.c.influxdb.com:8086/db/raptor',
+        username: 'heroku',
+        password: 'heroku'
+      },
+      'raptor-grafana': {
+        type: 'influxdb',
+        url: 'http://goldiewilson-onepointtwentyone-1.c.influxdb.com:8086/db/raptor-grafana',
+        username: 'heroku',
+        password: 'heroku',
+        grafanaDB: true
+      }
+    },
+    default_route: '/dashboard/file/raptor.json',
+    window_title_prefix: 'Raptor - ',
+    timezoneOffset: null,
+    grafana_index: 'grafana-dash',
+    unsaved_changes_warning: true,
+    plugins: {
+      panels: []
+    },
+    admin: 'ASK :ELI FOR ACTUAL PASSWORD'
+  });
+});
